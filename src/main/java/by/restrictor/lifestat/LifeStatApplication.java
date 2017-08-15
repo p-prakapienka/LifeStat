a0376e7048b51032bb34773434f8eef8bcc93ed6
Paviel Prakapienka <P.Prokopenko@gpsolutions.com>
2017-08-15 18:15:29 +0300

    0.0.1_Initiate

---
+++ src/main/java/by/restrictor/lifestat/LifeStatApplication.java
@@ -0,0 +1,13 @@
+package by.restrictor.lifestat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeStatApplication.class);
+    }
+
+}
