f1193a01a62b770c39bcc7933dc395c393b4c1ef
Paviel Prakapienka <P.Prokopenko@gpsolutions.com>
2017-08-23 20:13:55 +0300

    0.0.5_Totals_and_content_width

--- src/main/java/by/restrictor/lifestat/controller/RootController.java
+++ src/main/java/by/restrictor/lifestat/controller/RootController.java
@@ -1,6 +1,7 @@
 package by.restrictor.lifestat.controller;
 
 import static by.restrictor.lifestat.model.Categories.CATEGORIES;
+import static by.restrictor.lifestat.util.DoubleUtil.sum;
 
 import by.restrictor.lifestat.model.DailyStatement;
 import by.restrictor.lifestat.model.Income;
@@ -8,8 +9,12 @@ import by.restrictor.lifestat.model.Spending;
 import by.restrictor.lifestat.repository.IncomeRepository;
 import by.restrictor.lifestat.service.SpendingService;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -37,6 +42,9 @@ public class RootController {
 
         List<DailyStatement> dailyStatements = spendingService.getDailyStatements();
         model.addAttribute("dailySpendings", dailyStatements);
+
+        List<Double> categoryTotals = getCategoryTotals(dailyStatements);
+        model.addAttribute("catTotals", categoryTotals);
 
         double total = countTotal(dailyStatements);
         double incomes = incomeRepository.findAll().stream().findFirst().orElse(new Income()).getAmount();
@@ -79,6 +87,25 @@ public class RootController {
         return dailyStatements.stream()
             .mapToDouble(DailyStatement::getTotal)
             .sum();
+    }
+
+    private List<Double> getCategoryTotals(List<DailyStatement> dailyStatements) {
+        double[] sums = new double[CATEGORIES.size() + 1];
+        dailyStatements.forEach(ds -> {
+            sums[0] = sum(sums[0], ds.getFoodAmount());
+            sums[1] = sum(sums[1], ds.getCigAmount());
+            sums[2] = sum(sums[2], ds.getEntmtAmount());
+            sums[3] = sum(sums[3], ds.getTranspAmount());
+            sums[4] = sum(sums[4], ds.getHouseAmount());
+            sums[5] = sum(sums[5], ds.getCommAmount());
+            sums[6] = sum(sums[6], ds.getHobbyAmount());
+            sums[7] = sum(sums[7], ds.getMajorAmount());
+            sums[8] = sum(sums[8], ds.getDebtAmount());
+            sums[9] = sum(sums[9], ds.getRentAmount());
+            sums[10] = sum(sums[10], ds.getBankAmount());
+            sums[11] = sum(sums[11], ds.getTotal());
+        });
+        return Arrays.stream(sums).boxed().collect(Collectors.toList());
     }
 
 }
