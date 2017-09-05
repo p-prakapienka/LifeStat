68e740ab408450e5bd665388d59e907f91bfafd1
Paviel Prakapienka <P.Prokopenko@gpsolutions.com>
2017-09-05 16:18:35 +0300

    0.0.7_Fix csrf

--- src/main/java/by/restrictor/lifestat/config/SecurityConfig.java
+++ src/main/java/by/restrictor/lifestat/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${lifestat.user:user}")
@@ -23,7 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .httpBasic()
-        ;
+            .and()
+            .csrf().disable();
     }
 
     @Autowired
