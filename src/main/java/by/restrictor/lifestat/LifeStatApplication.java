76362ddaf55dcb8df8b1f7444af4848a510edadc
p-prakapienka <prakapienka.work@gmail.com>
2018-09-26 18:08:15 +0300

    0.0.8_local_deployment

--- src/main/java/by/restrictor/lifestat/LifeStatApplication.java
+++ src/main/java/by/restrictor/lifestat/LifeStatApplication.java
@@ -2,9 +2,16 @@ package by.restrictor.lifestat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LifeStatApplication {
+public class LifeStatApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LifeStatApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LifeStatApplication.class);
